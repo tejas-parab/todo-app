a9269c86fea3cea39ae8b1c98d2e3bf732543576
Tejas <tejasparab9673@gmail.com>
2025-11-02 00:48:31 +0530

    intial commit

---
+++ backend/models/todo.model.js
@@ -0,0 +1,16 @@
+import mongoose, { trusted } from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    text:{
+        type:String,
+        required: true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true})
+
+const Todo = mongoose.model("Todo",todoSchema);
+
+export default Todo;
