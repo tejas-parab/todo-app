0752afbe08678cd983bbb1cc2794bc451ed55a95
Tejas <tejasparab9673@gmail.com>
2025-11-02 01:14:35 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use("/api/todos",todoRoutes);
 const __dirname = path.resolve();
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(Path.json(__dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
     });
